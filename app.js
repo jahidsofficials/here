7d019098fb890de92e458adb4c5a89affebfa133
KDS CODER  (Jahid Khan) <76955321+jahidsofficials@users.noreply.github.com>
2022-09-20 01:12:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let download = document.getElementById('download');
+let download1 = document.getElementById('download1');
+let download2 = document.getElementById('download2');
+
+download.addEventListener('click', () => {
+    window.open('https://www.youtube.com/channel/UCzrG46-uiv5VG0_wa5zOjvA?sub_confirmation=1', "_blank");
+    download.style.display = 'none';
+    download1.innerText = 'Please Wait Checking Youtube Channel';
+    download1.style.display = "flex";
+    setTimeout(() => {
+        download1.innerText = '5 Sec Automatic Download';
+        setTimeout(() => {
+            download1.style.display = "none";
+            download2.style.display = "flex";
+        }, 5000);
+    }, 25000);
+})
